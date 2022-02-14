beda3f26839077db577726bcc1f3899824278992
Andrew <and_0@bk.ru>
2022-02-14 16:20:39 +0400

    Сделал Задачу 26: Возведите число А в натуральную степень B, используя цикл.

---
+++ Task_026_A to B power/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 26: Возведите число А в натуральную степень B, используя цикл.
+int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 5);
+int result = 1;
+for (int i = 0; i < b; i++)
+{
+    result = result * a;
+}
+Console.WriteLine($"Число {a} в степени {b} равно {result}");
