a0676a5d4171fc535f977779481131549e481884
Andrew <and_0@bk.ru>
2022-02-14 17:55:33 +0400

    Сделал Задачу 28: Подсчитайте сумму цифр в числе.

---
+++ Task_028_sum digits/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 28: Подсчитайте сумму цифр в числе.
+int number = new Random().Next(100, 10000);
+Console.WriteLine("Число: " + number);
+int sumDigits = 0;
+int deldigit = 0;
+while (number > 0)
+{
+    deldigit = number % 10;
+    number = number / 10;
+    sumDigits = sumDigits + deldigit;
+}
+Console.Write("Сумма цифр числа : "+sumDigits);
+
+
