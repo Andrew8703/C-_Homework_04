54482fc0090b89b99864db80541e0340a4b0d6e1
Andrew <and_0@bk.ru>
2022-02-15 22:37:11 +0400

    Сделал Задачу 32: Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран

---
+++ Task_032_array 8 elements/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 32: Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран
+int[] array = new int[8];
+
+int[] FillArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+        mass[i] = new Random().Next(0, 2);
+    return mass;
+}
+void PrintArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+        Console.Write($" {mass[i]} ");
+}
+FillArray(array);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
