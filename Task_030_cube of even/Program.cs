7a1a7cf437efea435e53e39e44570c9120440df7
Andrew <and_0@bk.ru>
2022-02-15 22:14:49 +0400

    Сделал Задачу 30: Покажите кубы чисел, заканчивающихся на чётную цифру

---
+++ Task_030_cube of even/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 30: Покажите кубы чисел, заканчивающихся на чётную цифру.
+int maxN = new Random().Next(0, 50);
+int minN = -1;
+Console.WriteLine($"Диапазон от {minN} до {maxN}");
+Console.WriteLine("Все кубы чисел, заканчивающихся на чётную цифру данного диапазона: ");
+while (minN <= maxN)
+{
+    if ((minN % 2) == 0)
+    {
+        Console.WriteLine($"Куб числа {minN} = {minN * minN * minN}");
+    }
+    minN++;
+}
